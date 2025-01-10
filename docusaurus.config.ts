7a1fd9ff7b6ae3d22ee84f7cbda420f57fefab55
Firas Darwish <f.darwish@diwink.com>
2025-01-11 01:40:24 +0300

    added algolia skeleton config, replaced logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,111 +5,148 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Ore',
-  tagline: 'Lightweight, Generic & Simple Dependency Injection Solution for Go',
-  favicon: 'img/favicon.ico',
+    title: 'Ore',
+    tagline: 'Lightweight, Generic & Simple Dependency Injection Solution for Go',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://ore.lilury.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://ore.lilury.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'firasdarwish', // Usually your GitHub org/user name.
-  projectName: 'ore', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'firasdarwish', // Usually your GitHub org/user name.
+    projectName: 'ore', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/lilury/ore-docs/tree/main/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/lilury/ore-docs/tree/main/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Ore',
-      logo: {
-        alt: 'Ore Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+    themeConfig: {
+        // algolia: {
+        //     // The application ID provided by Algolia
+        //     appId: 'YOUR_APP_ID',
+        //
+        //     // Public API key: it is safe to commit it
+        //     apiKey: 'YOUR_SEARCH_API_KEY',
+        //
+        //     indexName: 'YOUR_INDEX_NAME',
+        //
+        //     // Optional: see doc section below
+        //     contextualSearch: true,
+        //
+        //     // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //     externalUrlRegex: 'external\\.com|domain\\.com',
+        //
+        //     // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        //     replaceSearchResultPathname: {
+        //         from: '/docs/', // or as RegExp: /\/docs\//
+        //         to: '/',
+        //     },
+        //
+        //     // Optional: Algolia search parameters
+        //     searchParameters: {},
+        //
+        //     // Optional: path for search page that enabled by default (`false` to disable it)
+        //     searchPagePath: 'search',
+        //
+        //     // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        //     insights: true,
+        //
+        //     //... other Algolia params
+        // },
+        // Replace with your project's social card
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: true,
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/firasdarwish/ore',
-          label: 'GitHub',
-          position: 'right',
+        image: 'img/ore-gopher.png',
+        navbar: {
+            title: 'Ore',
+            logo: {
+                alt: 'Ore Logo',
+                src: 'img/ore-gopher.png',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Docs',
+                },
+                // {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://github.com/firasdarwish/ore',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: '/docs/intro',
-            },
-          ],
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Getting Started',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/ore',
+                        },
+                        {
+                            label: 'Linkedin',
+                            href: 'https://www.linkedin.com/company/lilury',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/xlilury',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Lilury. Built with Docusaurus.`,
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/ore',
-            },
-            {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/company/lilury',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/xlilury',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Lilury. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
