04e290e8c28db6caea8a91bec7b6caa64f797611
Firas Darwish <f.darwish@diwink.com>
2025-01-12 19:06:56 +0300

    changed title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} | Lilury`}
       description="Lightweight, Generic & Simple Dependency Injection Solution for Go">
       <HomepageHeader />
       <main>
